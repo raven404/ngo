2c649732744e066a80e6779aeb3073bd73a44014
raven404 <gky1232000@gmail.com>
2020-04-12 22:50:49 +0530

    url for class based views added

--- src/urls.py
+++ src/urls.py
@@ -3,8 +3,11 @@ from .import views
 app_name = 'src'
 
 urlpatterns = [
-    path('',views.index, name='home'),
-    path('blog/',views.blog, name='blog'),
+    # path('',views.index, name='home'),
+    # path('blog/',views.blog, name='blog'),
+    path('', views.IndexView.as_view(), name='home'),
+    # path('blog/', post_list, name='post-list'),
+    path('blog/', views.PostListView.as_view(), name='post-list'),
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),
     path(r'pay/',views.pay, name='pay'),

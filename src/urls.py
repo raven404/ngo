da5af8e67396b10f3538e27c3c7b15a92bc66e86
raven404 <gky1232000@gmail.com>
2020-04-14 22:43:49 +0530

    urls modified

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
+from subscribe.views import email_list_signup
 app_name = 'src'
 
 urlpatterns = [
@@ -13,5 +14,15 @@ urlpatterns = [
     path(r'pay/',views.pay, name='pay'),
     path(r'success/',views.success, name='success'),
     path(r'failure/',views.failure, name='failure'),
+
+    path('email-signup/', email_list_signup, name='email-list-signup'),
+    # path('create/', post_create, name='post-create'),
+    path('create/', views.PostCreateView.as_view(), name='post-create'),
+    # path('post/<id>/', post_detail, name='post-detail'),
+    path('post/<pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    # path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    # path('post/<id>/delete/', post_delete, name='post-delete'),
+    path('post/<pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
 ]

aadd997f36e6fb10f7271d9d4ef6130adae5bfc7
raven404 <gky1232000@gmail.com>
2020-04-15 22:53:34 +0530

    team model created

--- src/models.py
+++ src/models.py
@@ -21,6 +21,14 @@ class PostView(models.Model):
     def __str__(self):
         return self.user.username
 
+class TeamView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
 
 class Category(models.Model):
     title=models.CharField(max_length=20)
@@ -85,3 +93,20 @@ class Post(models.Model):
     @property
     def view_count(self):
         return PostView.objects.filter(post=self).count()
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = HTMLField()
+    # comment_count = models.IntegerField(default = 0)
+    view_count = models.IntegerField(default = 0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField(default=True)
+    # slider= models.BooleanField()
+    previous_post = models.ForeignKey(
+        'self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey(
+        'self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
